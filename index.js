554c0feb0de31480ecf4234769c6556a3681f569
salacryl <bjoern.meier@gmail.com>
2020-03-25 18:55:55 +0100

    test

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 // default route 
 app.get('/', (req, res, next) => {
-    res.render('homepage', {greeting: "Salacryl's starting project"});
+    res.render('homepage', {greeting: "Salacryl's starting project. Hallo "});
 })
 
 app.listen(PORT, () => logger.log('info', 'Webservice startet on Port: %d', PORT));
